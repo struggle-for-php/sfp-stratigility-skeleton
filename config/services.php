f15957c410d903efb413b49b7a52badd8e5a9b16
sasezaki <sasezaki@gmail.com>
2016-01-06 22:31:24 +0900

    removing SpfBackbeard, and add missing dependency(diactoros)

--- config/services.php
+++ config/services.php
@@ -1,8 +1,21 @@
 <?php
+use Backbeard\Dispatcher;
+
 return (new Zend\ServiceManager\ServiceManager)
 ->setService('Config', include 'parameters.php')
 ->setFactory('application', function($sm){
-    return new SfpBackbeard\Middleware($sm);
+    return function ($req, $res, $next) use ($sm) {
+        $routingFactory = $sm->get('routing-factory');
+        $view =$sm->get('view');
+        $stringRouter =$sm->get('string-router');
+
+        $dispatcher = new Dispatcher($routingFactory($sm), $view, $stringRouter);
+        $dispatchResult = $dispatcher->dispatch($req, $res);
+        if ($dispatchResult->isDispatched() !== false) {
+            return $dispatchResult->getResponse();
+        }
+        return $next($req, $res);
+    };
 })
 ->setFactory('routing-factory', function($sm){
     return function ($sm) {
@@ -10,6 +23,12 @@ return (new Zend\ServiceManager\ServiceManager)
             return 'hello';
         };
     };
+})
+->setFactory('view', function($sm){
+    return new Backbeard\View\SfpStreamView(new SfpStreamView\View(getcwd().'/views'));
+})
+->setFactory('string-router', function($sm){
+    return new Backbeard\Router\StringRouter(new \FastRoute\RouteParser\Std());
 })
 ->setFactory('ErrorHandler', function($sm){
     $displayErrors = ($sm->get('Config')['env'] !== 'production');
